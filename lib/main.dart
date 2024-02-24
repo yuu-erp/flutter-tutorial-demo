23d9f58e64cdb01e84006aec03bdd11cba499554
Lê Khải Hoàn <lekhaihoan@192.168.102.17>
2024-02-24 12:55:17 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Material(
+          child: Column(
+    children: [
+      Text('Xin chào1'),
+      Text('Xin chào2'),
+      Text('Xin chào2'),
+      Text('Xin chào2'),
+      Text('Xin chào2'),
+      Text('Xin chào2'),
+      Text('Xin chào2'),
+      Text('Xin chào2'),
+      Text('Xin chào2'),
+      Text('Xin chào2'),
+    ],
+  ))));
+}
